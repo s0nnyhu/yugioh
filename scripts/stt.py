a077907218e5ea008d149025dd6ec6c13dcb8825
s0nnyhu <husonny1@gmail.com>
2021-09-08 22:07:18 +0200

    Update script and data.json

--- scripts/stt.py
+++ scripts/stt.py
@@ -1,3 +1,5 @@
+import re
+
 with open('list_cards_number.txt') as f:
     lines = f.read().replace('\n', '')
     x = str(lines).replace('zéro', '0').replace('  ', ' ').split(' ')
@@ -6,13 +8,17 @@ with open('list_cards_number.txt') as f:
     x.sort()
     my_dict = {i: x.count(i) for i in x}
 
-print(len(x))
+print("Total: " + str(len(x)))
 with open('template_booster.txt', 'r') as file:
     # read a list of lines into data
     data = file.readlines()
 
-for i in range(0, 100):
-    nbCard = my_dict.get(i) if my_dict.get(i) else 0
+for i in range(0, len(data)):
+    # get number of card from id of the card from template
+    rawId = str(data[i].split(";")[0].split('-')[1])
+    idCard = str(re.findall('[0-9]+', rawId)[0]).lstrip('0') or '0'
+    nbCard = my_dict.get(int(idCard)) if my_dict.get(int(idCard)) else 0
+    # Replace
     data[i] = data[i].replace("\n", "") + "; " + str(nbCard) + "\n"
 
 with open('template_booster_updated.txt', 'w') as file:
